a2b05157043b0411c49a574f196bf43738ab5017
Woomy4680-exe <matzar1808@gmail.com>
2021-04-11 12:13:47 +0200

    CLI: Add usage() function

---
+++ src/usage.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+void
+usage(void)
+{
+	printf("Gobed: Sleep [NUMBER] (seconds (default)|minits|hours|days)\n");
+	printf("Usage: `gobed 1(m|h|d)` Sleep for 1 minit / hour / day\n");
+}
